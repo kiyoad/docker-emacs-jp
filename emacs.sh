aa8dacfa425e73c21eb79b36672e17439d918438
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-02-24 22:16:55 +0900

    Change docker detach sequence

--- emacs.sh
+++ emacs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -euo pipefail
-docker run --rm -it -v ${HOME}:/home/${LOGNAME} kiyoad/emacs $*
+docker run --rm -it -v ${HOME}:/home/${LOGNAME} --detach-keys="ctrl-q,ctrl-p" kiyoad/emacs $*
