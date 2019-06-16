43c7bb5ad36d5a0828273ad76bec21c6b031b6ad
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2019-06-16 10:36:48 +0900

    Remove unneeded sudo

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,8 @@ useradd -u "${uid}" -s /bin/bash "${logname}"
 echo "${logname} ALL=(ALL) NOPASSWD: ALL" > "/etc/sudoers.d/${logname}"
 chmod 0440 "/etc/sudoers.d/${logname}"
 
+chown "${logname}:${logname}" "/tmp/emacs${uid}"
+chmod go-rwx "/tmp/emacs${uid}"
+
 export TERM=xterm-256color
-sudo chown "${logname}:${logname}" "/tmp/emacs${uid}"
-sudo chmod go-rwx "/tmp/emacs${uid}"
 sudo -u "${logname}" -i /usr/local/bin/emacs "$@"
